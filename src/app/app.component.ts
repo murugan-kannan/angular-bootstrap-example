f3c5cc769099ed76841f9dc4ae6bfd885d281a60
murugan-kannan <machaendran9221@gmail.com>
2021-08-03 17:54:03 +0530

    3 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,21 @@
-import { Component, VERSION } from '@angular/core';
+import { AfterViewInit, Component, VERSION } from '@angular/core';
+import { Toast } from 'bootstrap';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent  {
-  
+export class AppComponent implements AfterViewInit {
+  ngAfterViewInit(): void {
+    Array.from(document.querySelectorAll('.toast')).forEach(
+      toastNode => new Toast(toastNode)
+    );
+  }
+
+  showToast() {
+    var myToastEl = document.getElementById('myToastEl');
+    var myToast = Toast.getInstance(myToastEl);
+    myToast.show();
+  }
 }
